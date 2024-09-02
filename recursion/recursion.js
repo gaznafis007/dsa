5c33ab1fa32e503c2650376148a50318819e5160
unknown <gazinafisrafi.gnr@gmail.com>
2024-09-02 16:18:10 +0600

    recursion practice

---
+++ recursion/recursion.js
@@ -0,0 +1,37 @@
+const recursiveHello = number =>{
+    if(number == 0){
+        return "END";
+    }
+    recursiveHello(number-1)
+    console.log(`hello ${number}`)
+}
+// console.log(recursiveHello(1))
+
+function sumOfN (number){
+    if(number === 0){
+        return 0;
+    }
+    // console.log(result)
+     let result = sumOfN(number-1) //solution vai utkarsh gupta
+    return number + result;
+}
+
+// console.log(sumOfN(10))
+
+function patternPrint (number){
+    if(number < 2){
+        console.log(1)
+        return;
+    }
+    for(let i = 1; i<=number; i++){
+        console.log(i)
+    }
+    console.log("")
+    patternPrint(number-1)
+    for (let i = 1; i<= number; i++){
+        console.log(i)
+    }
+    console.log("")
+}
+
+patternPrint(4)
