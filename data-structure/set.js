596a464e36ecbe4113f58ab8b8e08f38a0314d82
unknown <gazinafisrafi.gnr@gmail.com>
2024-09-09 11:54:45 +0600

    set data-structure

---
+++ data-structure/set.js
@@ -0,0 +1,11 @@
+const set = new Set([1,2,3,4]);
+console.log(set);
+
+set.add(5);
+console.log(set.has(6))
+console.log(set)
+console.log(set.size)
+// set.clear()
+set.delete(2)
+
+console.log(set)
