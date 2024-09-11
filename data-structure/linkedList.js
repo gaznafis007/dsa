ae45e6a59fd818fde1e3be0a19380e5d3d4cd2b2
unknown <gazinafisrafi.gnr@gmail.com>
2024-09-11 23:21:30 +0600

    prepend initiated

--- data-structure/linkedList.js
+++ data-structure/linkedList.js
@@ -6,20 +6,53 @@ class Node {
 }
 
 class List {
-    constructor(value){
-        this.size = value;
-        this.length = 0;
+    constructor(){
+        this.head = null;
+        this.size = 0;
     }
     isEmpty(){
-        return this.length === 0;
+        return this.size === 0;
     }
-    isFull(){
-        return this.length === this.size
+    getSize(){
+        return this.size
+    }
+    prepend(value){
+        const node = new Node(value);
+        if(this.isEmpty()){
+            this.head = node
+        }
+        else{
+            node.next = this.head;
+            this.head = node
+        }
+        this.size++
+        return this.head;
+    }
+    append(value){
+        
+    }
+    print(){
+        if(this.isEmpty()){
+            console.log("List is empty")
+        }
+        else{
+            let current = this.head;
+            let str = ``
+            while (current) {
+                str = `${str} ${current.value}`
+                current = current.next
+            }
+            console.log(str)
+        }
     }
 }
 
-const linkedList = new List(5);
+const linkedList = new List()
 
-console.log(linkedList.isEmpty());
-console.log(linkedList.isFull());
-
+// console.log(linkedList.isEmpty())
+// console.log(linkedList.getSize())
+linkedList.prepend(5)
+linkedList.prepend(10)
+console.log(linkedList.prepend(15))
+console.log(linkedList.getSize())
+linkedList.print()
